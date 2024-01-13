7e6ddf9ce5b34feed863c3fccb3bbae218d73e1f
Guilherme <francaguilherme27@gmail.com>
2024-01-13 11:01:40 -0300

    Criação da api via Spring Initializer

---
+++ SimpleBank-API/SimpleBank/src/test/java/br/com/tgidSimpleBank/SimpleBank/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tgidSimpleBank.SimpleBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
