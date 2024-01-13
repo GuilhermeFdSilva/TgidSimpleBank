06ca3a5dbdb4e6d2b92de4905c08a697db41ce2b
Guilherme <francaguilherme27@gmail.com>
2024-01-13 19:56:32 -0300

    Query findByCPF e findByCompanyId

--- SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/ClientRepository.java
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/ClientRepository.java
@@ -2,6 +2,13 @@ package br.com.tgidSimpleBank.SimpleBank.repositories;
 
 import br.com.tgidSimpleBank.SimpleBank.models.users.inherited.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    @Query("SELECT c FROM Client c WHERE c.cpf = :cpf")
+    public Client findByCpf(String cpf);
+    @Query("SELECT c FROM Client c WHERE c.company.id = :companyId")
+    public List<Client> findByCompanyId(Long companyId);
 }
