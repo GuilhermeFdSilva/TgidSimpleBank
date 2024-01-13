704f62a7fee0363936753bf734521f1c1b3d8c7a
Guilherme <francaguilherme27@gmail.com>
2024-01-13 19:56:52 -0300

    Query findByCnpj

--- SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/CompanyRepository.java
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/CompanyRepository.java
@@ -2,6 +2,9 @@ package br.com.tgidSimpleBank.SimpleBank.repositories;
 
 import br.com.tgidSimpleBank.SimpleBank.models.users.inherited.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    @Query("SELECT c FROM Company c WHERE c.cnpj = :cnpj")
+    public Company findByCnpj(String cnpj);
 }
