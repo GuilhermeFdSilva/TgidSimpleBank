d8e4066812c046ca2f8ba496616f1096b4a21895
Guilherme <francaguilherme27@gmail.com>
2024-01-13 19:57:09 -0300

    Query findByEmail

--- SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/UserRepository.java
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package br.com.tgidSimpleBank.SimpleBank.repositories;
 
 import br.com.tgidSimpleBank.SimpleBank.models.users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(String email);
 }
