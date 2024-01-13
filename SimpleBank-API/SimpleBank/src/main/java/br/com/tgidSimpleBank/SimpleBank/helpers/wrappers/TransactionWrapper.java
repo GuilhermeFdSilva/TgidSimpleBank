ed4c8c31496d95f35a7bfebe21c99c1c2c0ec3da
Guilherme <francaguilherme27@gmail.com>
2024-01-13 20:48:02 -0300

    Wrapper para obtenção de dados da requisição

---
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/helpers/wrappers/TransactionWrapper.java
@@ -0,0 +1,16 @@
+package br.com.tgidSimpleBank.SimpleBank.helpers.wrappers;
+
+import br.com.tgidSimpleBank.SimpleBank.models.users.inherited.Client;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionWrapper {
+    private Client author;
+    private Double value;
+}
