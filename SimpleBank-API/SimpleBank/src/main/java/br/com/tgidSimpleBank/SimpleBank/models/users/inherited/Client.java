11debd75cede4b5723998c876214a6d73e491b74
Guilherme <francaguilherme27@gmail.com>
2024-01-13 12:45:16 -0300

    Estruturação da entidade Cliente e Empresa

---
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/models/users/inherited/Client.java
@@ -0,0 +1,19 @@
+package br.com.tgidSimpleBank.SimpleBank.models.users.inherited;
+
+import br.com.tgidSimpleBank.SimpleBank.models.users.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client extends User {
+    @Column(nullable = false)
+    private String cpf;
+}
---
+++ SimpleBank-API/SimpleBank/src/main/java/br/com/tgidSimpleBank/SimpleBank/models/users/inherited/Company.java
@@ -0,0 +1,21 @@
+package br.com.tgidSimpleBank.SimpleBank.models.users.inherited;
+
+import br.com.tgidSimpleBank.SimpleBank.models.users.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "companies")
+public class Company extends User {
+    @Column(nullable = false)
+    private String cnpj;
+}
